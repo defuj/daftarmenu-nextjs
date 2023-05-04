b49ec80ca00608552afad53f77b89f8da74235ce
Dede Fuji Abdul <defuj.official@gmail.com>
2023-05-04 11:56:51 +0700

    fix index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import '../../helper/i18n';
 
 const inter = Inter({ subsets: ['latin'] })
 
